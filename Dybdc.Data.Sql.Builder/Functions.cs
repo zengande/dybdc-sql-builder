4ffacd9fad483845f8ba58f06cb3c5ad7f4fd429
zeng ande <zengande@outlook.com>
2020-12-21 17:53:16 +0800

    feat: Function 添加别名

--- Dybdc.Data.Sql.Builder/Functions.cs
+++ Dybdc.Data.Sql.Builder/Functions.cs
@@ -9,34 +9,34 @@ namespace Dybdc.Data.Sql.Builder
     /// </summary>
     public class Functions
     {
-        public static string Aggregate(string column, string function)
+        public static string Aggregate(string column, string function, string alias = null)
         {
             switch (function.ToUpper())
             {
                 case "MAX":
-                    return Max(column);
+                    return Max(column, alias);
                 case "MIN":
-                    return Min(column);
+                    return Min(column, alias);
                 case "AVG":
-                    return Avg(column);
+                    return Avg(column, alias);
                 case "SUM":
-                    return Sum(column);
+                    return Sum(column, alias);
                 case "COUNT":
-                    return Count(column);
+                    return Count(column, alias);
                 default:
                     throw new ArgumentException("聚合函数“{0}”不是有效的值", function);
             }
         }
 
-        public static string Max(string column) => string.Format("MAX({0})", column);
+        public static string Max(string column, string alias) => string.Format("MAX({0}) {1}", column, alias);
 
-        public static string Min(string column) => string.Format("MIN({0})", column);
+        public static string Min(string column, string alias) => string.Format("MIN({0}) {1}", column, alias);
 
-        public static string Avg(string column) => string.Format("AVG({0})", column);
+        public static string Avg(string column, string alias) => string.Format("AVG({0}) {1}", column, alias);
 
-        public static string Sum(string column) => string.Format("SUM({0})", column);
+        public static string Sum(string column, string alias) => string.Format("SUM({0}) {1}", column, alias);
 
-        public static string Count(string column) => string.Format("COUNT({0})", column);
+        public static string Count(string column, string alias) => string.Format("COUNT({0}) {1}", column, alias);
 
     }
 }
